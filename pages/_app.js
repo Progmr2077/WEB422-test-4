a9e298fa8fc01ed712a59d192db529aff3576efc
jrivera17 <jrivera17@myseneca.ca>
2025-03-14 15:35:24 -0400

    Added new fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import "@/styles/globals.css";
+import React from "react";
+import StudentForm from "../studentform.js";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
